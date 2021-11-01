4a7d1704f22b0c530c0bf66ac5b4217a4bf0e308
Mateus Henrique <mateushenriquechaves1@gmail.com>
2021-10-31 22:14:21 -0300

    review: last details fix

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
 
-const OPEN_WEATHER_API_APP_ID = '74e5e437ba09cbf2d96c21a8dcd21f0b';
-const GOOGLE_PLACES_API_KEY = 'AIzaSyDBAgxzPFtCQFyZqZeOYbaNIR1fgWx9XyE';
+const OPEN_WEATHER_API_APP_ID = '';
+const GOOGLE_PLACES_API_KEY = '';
 
 export default { BASE_URL, OPEN_WEATHER_API_APP_ID, GOOGLE_PLACES_API_KEY };
